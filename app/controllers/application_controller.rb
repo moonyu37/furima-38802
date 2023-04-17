0cc50ac361d96e398b6176f17ff4cfdfe92f782b
moonyu37 <yuzu.tink02@gmail.com>
2023-04-17 23:19:59 +0900

    サインアップ・ログイン機能の修正・RSpecの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-  end
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def authenticate_user!
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
-    end
+
+  private
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [ :nickname, :last_name, :first_name, :kana_last_name, :kana_first_name, :birthday])
+    end    
   end
-end
