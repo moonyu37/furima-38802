218cc28c43d3c7756dc021ae2278de6d160f4f33
moonyu37 <yuzu.tink02@gmail.com>
2023-05-04 17:53:34 +0900

    ビューの実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,6 +2,8 @@ class PurchasesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
   end
 
   def create
