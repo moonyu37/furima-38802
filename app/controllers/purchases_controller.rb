6b14050c4ff30db9b28a9e4345c8b242f16c2aab
moonyu37 <yuzu.tink02@gmail.com>
2023-05-06 01:27:09 +0900

    モデル・コントローラの修正/テストコード

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,5 +7,19 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+       @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:post_code, :sender_area_id, :municipalities, :street_address, :building_name, :phone_number, :image).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
---
+++ spec/models/purchase_address_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+RSpec.describe PurchaseAddress, type: :model do
+  before do
+    @purchase_address = FactoryBot.build(:purchase_address)
+  end
+
+  describe '配送先情報の保存' do
+    context '配送先が保存できる場合' do
+      it 'すべての項目が入力されていれば保存できる' do
+        expect(@purchase_address).to be_valid
+      end
+
+      it '建物名が空欄でも保存できる' do
+        @purchase_address.building_name = nil
+        expect(@purchase_address).to be_valid
+      end
+    end
+
+    context '配送先が保存できない場合' do
+      it '郵便番号が空欄では保存できない' do
+        @purchase_address.post_code = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
+      end
+
+      it '郵便番号にハイフンがなければ保存できない' do
+        @purchase_address.post_code = '1231234'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
+      end
+
+      it '都道府県の情報が未選択では保存できない' do
+        @purchase_address.sender_area_id = 1
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Sender area can't be blank")
+      end
+
+      it '市区町村が空欄では保存できない' do
+        @purchase_address.municipalities = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Municipalities can't be blank")
+      end
+
+      it '番地が空欄では保存できない' do
+        @purchase_address.street_address = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Street address can't be blank")
+      end
+
+      it '電話番号が空欄では保存できない' do
+        @purchase_address.phone_number = ''
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number is invalid")
+      end
+
+      it '電話番号にハイフンがあると保存できない' do
+        @purchase_address.phone_number = '090-0000-0000'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number is invalid")
+      end
+
+      it '電話番号が12桁以上では保存できない' do
+        @purchase_address.phone_number = '090000000000'
+        @purchase_address.valid?
+        expect(@purchase_address.errors.full_messages).to include("Phone number is invalid")
+      end
+
+    end
+  end
+end
