1c23b387eff12536fcc6188543965d249e70ede8
moonyu37 <yuzu.tink02@gmail.com>
2023-05-07 16:36:02 +0900

    "Sold out"の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,17 +1,17 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_purchase, only: [:index, :create]
+  before_action :existence_url, only: [:index, :create]
 
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
-      
+      pay_item
        @purchase_address.save
       redirect_to root_path
     else
@@ -21,16 +21,27 @@ class PurchasesController < ApplicationController
 
   private
 
+  def set_purchase
+    @item = Item.find(params[:item_id])
+  end
+
   def purchase_params
     params.require(:purchase_address).permit(:post_code, :sender_area_id, :municipalities, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
     Payjp::Charge.create(
-      amount: @item.price,  # 商品の値段
-      card: purchase_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      amount: @item.price,  
+      card: purchase_params[:token],  
+      currency: 'jpy'                 
     )
   end
+
+  def existence_url
+    if @item.user_id == current_user.id || @item.purchase != nil
+      redirect_to root_path
+    end
+  end
+
 end
